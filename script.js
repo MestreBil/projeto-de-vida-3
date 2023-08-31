d80698eb1fc45cf57090f206c967daa3157ceb72
mestre <gustavo.gugusan19@gmail.com>
2023-08-31 20:29:02 -0300

    projeto de vida

---
+++ script.js
@@ -0,0 +1,28 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
+
+addTaskButton.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        addTask(taskText);
+        taskInput.value = '';
+    }
+});
+
+function addTask(taskText) {
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = `
+        <span>${taskText}</span>
+        <button class="deleteTaskButton"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+      </svg>
+      </button>
+    `;
+    taskList.appendChild(taskItem);
+
+    const deleteButton = taskItem.querySelector('.deleteTaskButton');
+    deleteButton.addEventListener('click', () => {
+        taskList.removeChild(taskItem);
+    });
+}
